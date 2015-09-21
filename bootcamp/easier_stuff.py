44a23017f8a74ea9c6689c6b45e4e19408dc0c30
rugusf <student@otp584613ots.ucsf.edu>
2015-09-21 11:26:48 -0700

    might be right??

--- bootcamp/easier_stuff.py
+++ bootcamp/easier_stuff.py
@@ -50,14 +50,17 @@ def experiment():
 	
 	datalist = []
 	for row in data:
-		datalist.append[row]
+		datalist.append(row)
 	numrows = len(datalist)
 	numcols = len(datalist[1])
 	
 	maplist = []
-	for col in range(0, numcols):
-		for row in range(0, numrows):
-			maplist[.append
+	explist = []
+	for col in range(1, numcols):
+		for row in range(1, numrows):
+			pair = [datalist[row][0], datalist[row][col]]
+			explist.append(pair)
+		maplist.append(explist)
 	
 	txt.close()
 	
