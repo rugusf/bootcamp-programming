6c3bd5ba20a5c31dc92836211c48a0805217ed01
rugusf <student@android-3a9b78d05ee266fe.ucsf.edu>
2015-09-22 09:15:39 -0700

    gene name done!

--- bootcamp/data/test.py
+++ bootcamp/data/test.py
@@ -2,29 +2,28 @@ import os # a built-in module, for dealing with filenames
 #from . import app # this is part of the website guts
 import csv
 
-EXPERIMENT_FILE = "experiment_data.txt"
+GENE_INFO = "gene_info.txt"
 
-def experiment():
-	txt = open(EXPERIMENT_FILE)
+def gene_name(gene):
+	txt = open(GENE_INFO)
 	data = csv.reader(txt, delimiter = '\t')
 	
 	datalist = []
 	for row in data:
 		datalist.append(row)
 	numrows = len(datalist)
-	numcols = len(datalist[1])
 	
-	maplist = []
-	for col in range(1, numcols):
-		explist = []
-		for row in range(1, numrows):
-			pair = [datalist[row][0], datalist[row][col]]
-			explist.append(pair)
-		maplist.append(explist)
+	genelist = []
+	for row in range(1, numrows):
+		pair = [datalist[row][0], datalist[row][1]]
+		genelist.append(pair)
 	
-	print maplist
-	print '-' * 100
+	for i in range(0, numrows - 1):
+		if gene == genelist[i][0]:
+			return genelist[i][1]
+	
 	txt.close()
 
 
-experiment()
+print gene_name("YBR066C")
+print "should be NRG2"
